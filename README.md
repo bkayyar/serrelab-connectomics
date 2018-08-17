b2adb7edaec7b98f9e18c5556b49dc7624516614
Bharath <bharath_kayyar@brown.edu>
2018-08-17 02:58:42 -0400

    Wrote some README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # serrelab-connectomics
+
+Example commands to use conversion scripts. Look at the comments in each file for argument descriptions.
+
+Knossos to Numpy:
+python knossos_to_npy.py ./datasets/mag1/knossos.conf ./datasets/npy_files/exp_top.npy
+
+Numpy to Knossos:
+python npy_to_knossos.py ./datasets/npy_files/exp_numpy.npy ./datasets/ First-Experiment
+
+NOTE: The npy_to_knossos.py script automatically creates the top-level Knossos dataset directory. ~Do Not~ supply the
+name of an uncreated top-level directory.
--- conversion_scripts/knossos_to_npy.py
+++ conversion_scripts/knossos_to_npy.py
@@ -1,3 +1,20 @@
 import sys
 import numpy
 from knossos_utils import knossosdataset as knosdata
+    
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        raise RuntimeError('Usage: python knossos_to_npy.py <path_to_knossos_conf_file> <path_to_output_npy_file>')
+
+    conf_file = sys.argv[1] 
+    npy_file = sys.argv[2]
+
+    print("Initializing Knossos dataset...")
+    kd = knosdata.KnossosDataset()
+    kd.initialize_from_knossos_path(conf_file)
+    #Conversion to numpy array probably unnecessary but I'm yet to explore how numpy.save treats non-numpy arrays
+    print("Getting volume from raw data...")
+    vol = numpy.asarray(kd.from_raw_cubes_to_matrix(size=kd.boundary, offset=[0, 0, 0])) 
+    print("Saving numpy file...")
+    numpy.save(npy_file, vol)
+    print("Numpy file written!")
